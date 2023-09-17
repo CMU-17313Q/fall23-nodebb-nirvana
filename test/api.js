9de6a1be83daa6db78dde5348c36d7cc6adcda2f
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2023-09-17 20:05:30 +0300

    fix boolean bug

--- test/api.js
+++ test/api.js
@@ -296,7 +296,7 @@ describe('API', async () => {
                     }
 
                     const normalizedPath = pathObj.path.replace(/\/:([^\\/]+)/g, '/{$1}').replace(/\?/g, '');
-                    if (pathObj.path.startsWith('/forum/debug/')) {
+                    if (!pathObj.path.startsWith('/forum/debug/')) {
                         assert(schema.paths.hasOwnProperty(normalizedPath), `${pathObj.path} is not defined in schema docs`);
                         assert(schema.paths[normalizedPath].hasOwnProperty(pathObj.method), `${pathObj.path} was found in schema docs, but ${pathObj.method.toUpperCase()} method is not defined`);
                     }
